fe238c53ed04b64fe469a418c82f5a16697971d6
xSeRgio16 <secretohgjg@gmail.com>
2022-11-14 12:27:33 +0100

    Campos

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
 import javax.swing.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        Connection con = null;
+        Connection con;
+        String consulta = "INSERT INTO libros VALUES (null,?,?,?)";
+        PreparedStatement ps;
         try {
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/biblioteca","root", "");
-            JOptionPane.showMessageDialog(null,"la conexion si funciona :)");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/biblioteca", "root", "");
+            JOptionPane.showMessageDialog(null, "La conexión si funciona :)");
+            ps = con.prepareStatement(consulta);
+            ps.setString(1, "Harry Potter");
+            ps.setString(2, "Sergio");
+            ps.setString(3, "12/09/1994");
+            ps.execute();
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null,"la conexion no funciona :(");
+            JOptionPane.showMessageDialog(null, "La conexión no funciona :(");
         }
     }
 }
