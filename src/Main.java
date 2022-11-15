1ac768498356017737796bc43b480922a15c63e4
xSeRgio16 <secretohgjg@gmail.com>
2022-11-15 12:28:43 +0100

    select/delete

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,34 @@ public class Main {
     public static void main(String[] args) {
         Connection con;
         String consulta = "INSERT INTO libros VALUES (null,?,?,?)";
-        PreparedStatement ps;
+        String select = "SELECT * FROM libros";
+        String delete = "DELETE FROM libros WHERE nombre=?";
+        PreparedStatement psc, pss, psd;
+        ResultSet rs = null;
+        String campo;
         try {
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/biblioteca", "root", "");
             JOptionPane.showMessageDialog(null, "La conexión si funciona :)");
-            ps = con.prepareStatement(consulta);
+            psc = con.prepareStatement(consulta);
             String nombre = JOptionPane.showInputDialog(null, "Introduzca el nombre del libro");
-            ps.setString(1, nombre);
+            psc.setString(1, nombre);
             String autor = JOptionPane.showInputDialog(null, "Introduzca el nombre del autor");
-            ps.setString(2, autor);
+            psc.setString(2, autor);
             String fechapublicacion = JOptionPane.showInputDialog(null, "Introduzca la fecha de publicación");
-            ps.setString(3, fechapublicacion);
-            ps.execute();
+            psc.setString(3, fechapublicacion);
+            psc.execute();
+            pss = con.prepareStatement(select);
+            rs = pss.executeQuery();
+            while (rs.next()) {
+                String nombre2 = rs.getString("Nombre");
+                String autor2 = rs.getString("Autor");
+                String fechapublicacion2 = rs.getString("Fecha_publicacion");
+                System.out.print(" | " + nombre2);
+                System.out.print(" | " + autor2);
+                System.out.print(" | " + fechapublicacion2);
+                System.out.println();
+            }
+            psd = con.prepareStatement(delete);
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "La conexión no funciona :(");
         }
